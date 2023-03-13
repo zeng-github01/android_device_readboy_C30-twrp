53e86a06c6b1fe8260a5e1aec809eb826bf984c9
zengyj <zengyj11111@gmail.com>
2023-03-13 22:24:06 +0800

    remove obsolete code

--- device.mk
+++ device.mk
@@ -21,11 +21,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     bootctrl.msm8998
 
-PRODUCT_STATIC_BOOT_CONTROL_HAL := \
-    bootctrl.msm8998 \
-    libgptutils \
-    libz \
-    libcutils
+# PRODUCT_STATIC_BOOT_CONTROL_HAL := \
+#     bootctrl.msm8998 \
+#     libgptutils \
+#     libz \
+#     libcutils
 
 PRODUCT_PACKAGES += \
     otapreopt_script \
